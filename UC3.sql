f960f3030c9c37e70f7109a1f642d7fc387ad732
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-04 00:21:44 +0530

    employee payroll data

---
+++ UC3.sql
@@ -0,0 +1,2 @@
+use employee_payroll;
+ INSERT INTO employee_payroll_table( name, salary, start) VALUE ('Rohit', 1000000.00, '2018-01-03' ),('ABD', 2000080.00, '2019-11-13'),('Vk', 3000000.00, '2020-05-21' );
