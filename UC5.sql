520aa90cb6aa3e3c8b53381397436180d8ca228e
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-05-09 12:08:28 +0530

    Retrieve salary data for a particular employee

---
+++ UC5.sql
@@ -0,0 +1,3 @@
+use employee_payroll;
+select salary FROM employee_payroll_table WHERE name = 'ABD'; 
+    select * from employee_payroll_table  WHERE start BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW());
